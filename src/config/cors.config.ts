b4d62cc4e2d1485c0bdefb62f7bc19e6f1ba0b69
Shiroo <hmegxva@gmail.com>
2025-04-14 20:36:21 +0700

    update: add patch method for cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,6 +2,6 @@ import cors from 'cors'
 export const corsConfig = cors({
   origin: process.env.HOST_FE || 'http://localhost:3000',
   credentials: true, // Nếu có dùng cookies hoặc session
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // các phương thức HTTP cho phép
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // các phương thức HTTP cho phép
   allowedHeaders: ['Content-Type', 'Authorization'] // các headers cho phép
 })
